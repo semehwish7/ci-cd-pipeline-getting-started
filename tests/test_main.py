294e1c01f18960a21cec149303fd082e77afcea5
mehwish7 <se.mehwish7@gmail.com>
2025-11-06 11:54:43 +0500

    subtract func create and new testcases

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,11 @@
-from src.main import add
+from src.main import add,subtract
+
 def test_add_function():
     assert add(1, 2) == 3
-    assert add(0, 0) == 1
-    assert add(1, 9) ==   10
+    assert add(0, 0) == 0
+    assert add(1, 9) == 10
+
+def test_subtract_function():
+    assert subtract(6, 2) == 4
+    assert subtract(0, 0) == 0
+    assert subtract(10, 9) == 1
